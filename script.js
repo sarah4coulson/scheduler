e7dbd05239fc7488bbb52b57edf54ddf75a65c41
Sarah Coulson <scoulson2023@gmail.com>
2021-07-15 00:33:26 -0500

    script.js restart

--- script.js
+++ script.js
@@ -1,145 +1,46 @@
-$(document).ready(function () {
+var nine = document.getElementById("hour-9");
+var ten = document.getElementById("hour-10");
+var eleven = document.getElementById("hour-11");
+var twelve = document.getElementById("hour-12");
+var thirteen = document.getElementById("hour-13");
+var fourteen = document.getElementById("hour-14");
+var fifteen = document.getElementById("hour-15");
+var sixteen = document.getElementById("hour-16");
 
-    const test = false;
+var nineA = document.getElementById("9").innerText;
+console.log(nineA);
 
-    const now = moment().format('MMMM Do YYYY');
+var tenA = document.getElementById("10").innerText;
+console.log(tenA);
 
-    let nowHour24 = moment().format('H');
-    let nowHour12 = moment().format('h');
+var eleA = document.getElementById("11").innerText;
 
-    if (test) {
-        nowHour24 = 13;
-        nowHour12 = 1;
-    }
+var tweA = document.getElementById("12").innerText;
 
-    let $dateHeading = $('#currentDay');
-    $dateHeading.text(now);
+var thirtA = document.getElementById("13").innerText;
 
+var fourtA = document.getElementById("14").innerText;
 
-    let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+var fiftA = document.getElementById("15").innerText;
 
-    if (test) { console.log(storedPlans); }
+var sixtenA = document.getElementById("16").innerText;
 
+var sevenA = document.getElementById("17").innerText;
 
-    if (storedPlans !== null) {
-        planTextArr = storedPlans;
-    } else {
-        planTextArr = new Array(9);
-        planTextArr[4] = "Picnic lunch outside";
-    }
+var na = parseInt(nineA);
+var ta = parseInt(tenA);
+var ea = parseInt(eleA);
+var twa = parseInt(tweA);
+var tha = parseInt(thirtA) + 12;
+var foa = parseInt(fourtA) + 12;
+var fia = parseInt(fiftA) + 12;
+var sia = parseInt(sixtenA) + 12;
+var sea = parseInt(sevenA) + 12;
 
-    if (test) { console.log("full array of plned text", planTextArr); }
+var currentHr = parseInt(moment().format("HH"));
+console.log(currentHr);
+console.log(sea);
 
-    let $plannerDiv = $('#plannerContainer');
-    $plannerDiv.empty();
+if () {
 
-    if (test) { console.log("current time", nowHour12); }
-
-
-    for (let hour = 9; hour <= 17; hour++) {
-        let index = hour - 9;
-
-        let $rowDiv = $('<div>');
-        $rowDiv.addClass('row');
-        $rowDiv.addClass('plannerRow');
-        $rowDiv.attr('hour-index', hour);
-
-        let $col2TimeDiv = $('<div>');
-        $col2TimeDiv.addClass('col-md-2');
-
-        const $timeBoxSpn = $('<span>');
-        $timeBoxSpn.attr('class', 'timeBox');
-
-        let displayHour = 0;
-        let ampm = "";
-        if (hour > 12) {
-            displayHour = hour - 12;
-            ampm = "pm";
-        } else {
-            displayHour = hour;
-            ampm = "am";
-        }
-
-        $timeBoxSpn.text(`${displayHour} ${ampm}`);
-
-        $rowDiv.append($col2TimeDiv);
-        $col2TimeDiv.append($timeBoxSpn);
-
-
-        let $dailyPlanSpn = $('<input>');
-
-        $dailyPlanSpn.attr('id', `input-${index}`);
-        $dailyPlanSpn.attr('hour-index', index);
-        $dailyPlanSpn.attr('type', 'text');
-        $dailyPlanSpn.attr('class', 'dailyPlan');
-
-        $dailyPlanSpn.val(planTextArr[index]);
-
-        let $col9IptDiv = $('<div>');
-        $col9IptDiv.addClass('col-md-9');
-
-        $rowDiv.append($col9IptDiv);
-        $col9IptDiv.append($dailyPlanSpn);
-
-        let $col1SaveDiv = $('<div>');
-        $col1SaveDiv.addClass('col-md-1');
-
-        let $saveBtn = $('<i>');
-        $saveBtn.attr('id', `saveid-${index}`);
-        $saveBtn.attr('save-id', index);
-        $saveBtn.attr('class', "far fa-save saveIcon");
-
-        $rowDiv.append($col1SaveDiv);
-        $col1SaveDiv.append($saveBtn);
-
-        updateRowColor($rowDiv, hour);
-
-        $plannerDiv.append($rowDiv);
-    };
-
-    function updateRowColor($hourRow, hour) {
-
-        if (test) { console.log("rowColor ", nowHour24, hour); }
-
-        if (hour < nowHour24) {
-            if (test) { console.log("lessThan"); }
-            $hourRow.css("background-color", "lightgrey")
-        } else if (hour > nowHour24) {
-            if (test) { console.log("greaterthan"); }
-            $hourRow.css("background-color", "lightgreen")
-        } else {
-            if (test) { console.log("equal"); }
-            $hourRow.css("background-color", "tomato")
-        }
-    };
-
-    $(document).on('click', 'i', function (event) {
-        event.preventDefault();
-
-        if (test) { console.log('click pta before ' + planTextArr); }
-
-        let $index = $(this).attr('save-id');
-
-        let inputId = '#input-' + $index;
-        let $value = $(inputId).val();
-
-        planTextArr[$index] = $value;
-
-
-        if (test) { console.log('value ', $value); }
-        if (test) { console.log('index ', $index); }
-        if (test) { console.log('click pta after ' + planTextArr); }
-
-
-    });
-
-    $(document).on('change', 'input', function (event) {
-        event.preventDefault();
-        if (test) { console.log('onChange'); }
-        if (test) { console.log('id', $(this).attr('hour-index')); }
-
-        let i = $(this).attr('hour-index');
-
-        $(`#saveid-${i}`).addClass('shadowPulse');
-    });
-});
+};
