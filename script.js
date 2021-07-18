9559b0c4fbfcbaef6200217249537a7a1e0633e1
Sarah Coulson <scoulson2023@gmail.com>
2021-07-18 12:55:13 -0600

    editing final javascript

--- script.js
+++ script.js
@@ -168,13 +168,3 @@ if (sea === currentHr) {
   seventeen.classList.remove("past");
   seventeen.classList.add("future");
 }
-
-$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-$("#hour-17 .description").val(localStorage.getItem("hour-17"));
